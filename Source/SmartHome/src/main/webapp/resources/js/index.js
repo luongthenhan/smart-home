267f46b65bd1b6bac32744327d985518f74e28d2
nttung1994 <tung.cs.1994@gmail.com>
2016-07-30 11:04:05 +0700

    - Update layout for camera

--- Source/SmartHome/src/main/webapp/resources/js/index.js
+++ Source/SmartHome/src/main/webapp/resources/js/index.js
@@ -1,3 +1,7 @@
 function toggleExpandIconOfLightBlub() {
 	$(".btn_expand_light_bulb span").toggleClass('fa-chevron-right fa-chevron-down');
 }
+
+function toggleExpandIconOfCamera(){
+	$(".btn_expand_camera span").toggleClass('fa-chevron-right fa-chevron-down');
+}
