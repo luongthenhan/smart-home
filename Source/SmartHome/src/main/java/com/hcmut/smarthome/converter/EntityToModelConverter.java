55710a78023e0df9e6e67587ba21d3d97b391c61
nttung1994 <tung.cs.1994@gmail.com>
2016-09-24 11:20:28 +0700

    - Update entity to model converter: Refactor code

--- Source/SmartHome/src/main/java/com/hcmut/smarthome/converter/EntityToModelConverter.java
+++ Source/SmartHome/src/main/java/com/hcmut/smarthome/converter/EntityToModelConverter.java
@@ -1,6 +1,5 @@
-package com.hcmut.smarthome.converter.impl;
+package com.hcmut.smarthome.converter;
 
-import com.hcmut.smarthome.converter.IEntityToModelConverter;
 import com.hcmut.smarthome.entity.Device;
 import com.hcmut.smarthome.model.Buzzer;
 import com.hcmut.smarthome.model.Camera;
@@ -10,39 +9,32 @@ import com.hcmut.smarthome.model.LightSensor;
 import com.hcmut.smarthome.model.MotionSensor;
 import com.hcmut.smarthome.model.TemperatureSensor;
 
-public class EntityToModelConverterImpl implements IEntityToModelConverter {
+public class EntityToModelConverter {
 
-	@Override
 	public LightBulb convertToLightBulbModel(Device device) {
 		return new LightBulb(device);
 	}
 
-	@Override
 	public Buzzer convertToBuzzerModel(Device device) {
 		return new Buzzer(device);
 	}
 
-	@Override
 	public Camera convertToCameraModel(Device device) {
 		return new Camera(device);
 	}
 
-	@Override
 	public LightSensor convertToLightSensorModel(Device device) {
 		return new LightSensor(device);
 	}
 
-	@Override
 	public MotionSensor convertToMotionSensorModel(Device device) {
 		return new MotionSensor(device);
 	}
 
-	@Override
 	public TemperatureSensor convertToTemperatureSensorModel(Device device) {
 		return new TemperatureSensor(device);
 	}
 
-	@Override
 	public GasSensor convertToGasSensorModel(Device device) {
 		return new GasSensor(device);
 	}
