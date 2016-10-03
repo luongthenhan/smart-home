7730ad9442308509dd2e2e470f50cca804032ca2
darkomega9408 <darkomega9408@yahoo.com>
2016-10-03 21:58:53 +0700

    + Add multiple modes for device's scripts

--- Source/SmartHomeWebMobile/www/app/shared/device-panel/devicePanelController.js
+++ Source/SmartHomeWebMobile/www/app/shared/device-panel/devicePanelController.js
@@ -1,4 +1,4 @@
-app.directive("devicePanel", function() {
+app.directive("devicePanel", ['MainService', function(MainService) {
     return {
         restrict: "E",
         scope: {
@@ -10,8 +10,17 @@ app.directive("devicePanel", function() {
         controllerAs: "devicePanelCtrl",
         controller: function($scope) {
             var self = this;
+            self.isShowDetails = false;
+
+            self.modes = [];
+            self.selectedMode = null;
+
+            self.toggleShowDetails = function() {
+                self.isShowDetails = !self.isShowDetails;
+            }
 
             self.init = function() {
+                modes = MainService.getModes(self);
                 $(".panel-collapse").on('hide.bs.collapse', function () {
                     var deviceId = $(this).attr("id").slice(-1);
                     $("#devicePanelToggleIcon" + deviceId).removeClass("glyphicon-menu-down");
@@ -28,6 +37,6 @@ app.directive("devicePanel", function() {
 
         }
     }
-})
+}])
 
 
