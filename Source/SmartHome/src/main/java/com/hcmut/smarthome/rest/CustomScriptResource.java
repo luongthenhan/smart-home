9a64ae94b60bbace8ed1a3bb618132563c56fdaa
Vinh <anhbikanhdeptrai@gmail.com>
2016-11-11 20:47:15 +0700

    Add new API , used to get list of device id of a specific script For example:
    Method: POST
    URL: http://localhost:8080/smarthome/api/homes/{homeId}/scripts
    Body:
    {
        "content": "[['If',['5','>=','21'],[['TurnOff','4']]]]",
        "type": {
          "id": 1
        }
    }
    
    --> Return:
    [
      4,
      5
    ]

---
+++ Source/SmartHome/src/main/java/com/hcmut/smarthome/rest/CustomScriptResource.java
@@ -0,0 +1,35 @@
+package com.hcmut.smarthome.rest;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hcmut.smarthome.model.Script;
+import com.hcmut.smarthome.service.IDeviceService;
+
+@RestController
+@RequestMapping("homes/{homeId}/scripts")
+@CrossOrigin
+public class CustomScriptResource {
+
+	@Autowired
+	private IDeviceService deviceService;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Set<Integer>> getListDeviceId( @PathVariable int homeId, @RequestBody Script script) {
+		try{
+			return new ResponseEntity<Set<Integer>>(deviceService.getListDeviceIdInScript(homeId, script), HttpStatus.OK );
+		}
+		catch(Exception e){
+			return new ResponseEntity<Set<Integer>>(HttpStatus.BAD_REQUEST);
+		}
+	} 
+}
