c72f5072d77c95a8a09b024b63562470aa8e179b
nttung1994 <tung.cs.1994@gmail.com>
2016-12-28 18:46:16 +0700

    - Fix bug: cannot register

--- Source/SmartHomeWebMobile/www/app/components/login/loginController.js
+++ Source/SmartHomeWebMobile/www/app/components/login/loginController.js
@@ -17,6 +17,7 @@ app.controller('LoginController', ['MainService', '$location', function(MainServ
     }
 
     self.register = function() {
+        MainService.hostDomain = self.hostDomain;
         $location.path("/register");
     }
 }])
